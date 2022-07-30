c4fb054bbd4c9a10bd83180592f9b129b4afd932
krishnaTech6 <krishna060702@gmail.com>
2022-07-30 21:29:22 +0530

    improving the user experience

--- app/src/main/java/com/example/thequizapp/QuizWindow.kt
+++ app/src/main/java/com/example/thequizapp/QuizWindow.kt
@@ -3,6 +3,7 @@ package com.example.thequizapp
 import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Color.*
 import androidx.appcompat.app.AppCompatActivity
@@ -18,10 +19,12 @@ import androidx.core.content.withStyledAttributes
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import java.sql.Time
+import kotlin.system.exitProcess
 
 class QuizWindow : AppCompatActivity(), View.OnClickListener {
 
     var score:Int=0
+    var i: Int = 0
     var totalQuestion= QuestionAnswers.questions.size
     var currentQuestionIndex:Int =0
     var selectedAns: String =""
@@ -77,71 +80,69 @@ class QuizWindow : AppCompatActivity(), View.OnClickListener {
                 score++ }
             else{score }  //otherwise it was sometimes showing score to 1, if no option was clicked
 
-            if (selectedAns == actualAnswer ){
-                hello.setBackgroundColor(GREEN)
+            when (selectedAns) {
+                actualAnswer -> {
+                    hello.setBackgroundColor(GREEN) }
+                "" -> {
+                    hello.setBackgroundColor(WHITE)
+                    if (i==0) {Toast.makeText(this, "No Option Chosen", Toast.LENGTH_SHORT).show()
+                    i=2} // i is used to show toast one time only if multiple time button is clicked
                 }
-            else{
-                hello.setBackgroundColor(RED)
+                else -> {
+                    hello.setBackgroundColor(RED)
+                    when(actualAnswer){   //to show the right answer after submit
+                        ansa.text.toString() -> ansa.setBackgroundColor(GREEN)
+                        ansb.text.toString() -> ansb.setBackgroundColor(GREEN)
+                        ansc.text.toString() -> ansc.setBackgroundColor(GREEN)
+                        ansd.text.toString() -> ansd.setBackgroundColor(GREEN)
+                    }
+                }
             }
 
-            when(actualAnswer){
-                ansa.text.toString() -> ansa.setBackgroundColor(GREEN)
-                ansb.text.toString() -> ansb.setBackgroundColor(GREEN)
-                ansc.text.toString() -> ansc.setBackgroundColor(GREEN)
-                ansd.text.toString() -> ansd.setBackgroundColor(GREEN)
-            }
+
+
             //Toast.makeText(this , "Answer: $actualAnswer \n score: $score/$totalQuestion ", Toast.LENGTH_SHORT).show()
 
-            submitbtn.visibility = View.INVISIBLE
 
-            Handler(Looper.getMainLooper()).postDelayed(
-                {
+            if (selectedAns =="") {
+                currentQuestionIndex}
+            else {
+                currentQuestionIndex++
+                i=0
+                submitbtn.visibility = View.INVISIBLE
 
-                    currentQuestionIndex++
+                Handler(Looper.getMainLooper()).postDelayed(
+                    {
+                        if (currentQuestionIndex >= totalQuestion-1){
+                            submitbtn.text = "Submit"
+                        }
 
-                    if (currentQuestionIndex >= totalQuestion-1){
-                        submitbtn.text = "Submit"
-                    }
+                        loadNewQuestions()
+                        ansa.setBackgroundColor(WHITE)
+                        ansb.setBackgroundColor(WHITE)
+                        ansc.setBackgroundColor(WHITE)
+                        ansd.setBackgroundColor(WHITE)
 
-                    loadNewQuestions()
-                     ansa.setBackgroundColor(WHITE)
-                     ansb.setBackgroundColor(WHITE)
-                     ansc.setBackgroundColor(WHITE)
-                     ansd.setBackgroundColor(WHITE)
-
-                    submitbtn.visibility = View.VISIBLE
-
-
-                },
-                2000 // value in milliseconds
-            )
-
-
-
-
-
-
+                        submitbtn.visibility = View.VISIBLE
+                    },
+                    1000 // value in milliseconds
+                )
+            }
         }
+
         else{
             selectedAns = clickedButton.text.toString()
             clickedButton.setBackgroundColor(MAGENTA)
             hello = clickedButton
-
-
-//            button shows green color if right else red
-//            if (selectedAns == QuestionAnswers.answers[currentQuestionIndex] ){
-//                clickedButton.setBackgroundColor(GREEN) }
-//            else{
-//                clickedButton.setBackgroundColor(RED)
-//            }
-
-
         }
     }
 
 
     @SuppressLint("SetTextI18n")
     private fun loadNewQuestions(){
+
+        selectedAns =""  //so that when no option is chosen then string remains empty -> doesn't contain previous value
+                        // doesn't show red color on submit button press ___ LINE 83
 
         if (currentQuestionIndex == totalQuestion){
             finishQuiz()
@@ -171,10 +172,12 @@ class QuizWindow : AppCompatActivity(), View.OnClickListener {
         AlertDialog.Builder(this)
             .setTitle(passStatus)
             .setMessage("Score is $score out of $totalQuestion")
+            .setNegativeButton("Exit"){_,_ -> exitQuiz()}
             .setPositiveButton("Restart") { _, _ -> restartQuiz() }
             .setCancelable(false)
             .show() 
     }
+
      @SuppressLint("SetTextI18n")
      private fun restartQuiz() {
         score=0
@@ -183,8 +186,14 @@ class QuizWindow : AppCompatActivity(), View.OnClickListener {
         loadNewQuestions()
     }
 
+    private fun exitQuiz(){
+        finish()
+        exitProcess(0)
+    }
+
 //So that user can't go back during quiz
     override fun onBackPressed() {
+        AlertDialog.Builder(this).setMessage("Quiz is Running!!").setNegativeButton("OK", null).show()
     }
 }
 
